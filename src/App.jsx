41550323b65c7531a1e57679dee13895501b2186
saharshbaiju <saharshbaiju@gmail.com>
2026-04-08 16:16:27 +0530

    feat: front page | seach input

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 // import {Routes,Route} from "react-router-dom";
 
+import Front from "./Front";
+
 function App() {
   return (
     <>
-    
+    <Front/>
     </>
   )
 }
 
-export default App
+export default App;
---
+++ src/components/background/Background.jsx
@@ -0,0 +1,11 @@
+import "./Background.css"
+
+export default function Background(){
+
+    return(
+        <div className="background">
+            <div className="blue circle1"></div>
+        </div>
+        
+    );
+}
