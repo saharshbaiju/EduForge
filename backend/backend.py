acac913538ff84af979a9bde95df42be6a7e0ddc
saharshbaiju <saharshbaiju@gmail.com>
2026-04-16 18:34:48 +0530

    feat: backend boiler plate and db connection

--- backend/backend.py
+++ backend/backend.py
@@ -1,59 +1,29 @@
 from flask import Flask
-
 import requests
+from werkzeug.security import generate_password_hash
+from werkzeug.security import check_password_hash
+import mysql.connector
+from flask_cors import CORS
+from dotenv import load_dotenv
+import os
 
-url = "https://www.googleapis.com/youtube/v3/search"
-params = {
-    "part": "snippet",
-    "q": "machine learning",
-    "type": "video",
-    "maxResults": 5,
-    "key":
-}
-    # "key": "AIzaSyCCRU7MzJx85y_QID0fXAUwqaRaUO4hBtU"
-res = requests.get(url, params=params)
-print(res.json())
+app = Flask(__name__)
 
+load_dotenv()
 
+def get_db():
+    return mysql.connector.connect(
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME"),
+        autocommit=os.getenv("DB_AUTOCOMMIT") == "True"
+    )
 
+db = get_db()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+CORS(app, origins=["http://localhost:5173"])
+cursor = db.cursor()
 
 
 
